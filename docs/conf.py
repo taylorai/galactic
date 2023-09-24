e7004a57981bbc931197b5c3157f5fac0df4c054
Benjamin Anderson <andersonbcdefg@gmail.com>
2023-09-24 00:55:19 -0700

    add api doc rst

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath("../src"))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
